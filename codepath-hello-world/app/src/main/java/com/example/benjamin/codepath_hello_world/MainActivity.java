429d5c0b656dff3d616569f33ef639d5993f91c9
Benjamin Warner <benjaminwarner@u.boisestate.edu>
2018-09-07 16:56:14 -0600

    Change text color on button click.

--- codepath-hello-world/app/src/main/java/com/example/benjamin/codepath_hello_world/MainActivity.java
+++ codepath-hello-world/app/src/main/java/com/example/benjamin/codepath_hello_world/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.benjamin.codepath_hello_world;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.changeTextColorButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("Ben", "Button click!");
+                TextView helloTextView = findViewById(R.id.helloTextView);
+                helloTextView.setTextColor(getResources().getColor(R.color.colorPrimaryDark));
+            }
+        });
     }
 }
