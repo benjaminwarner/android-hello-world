2e00a4e6c37acaea17bdc4f3f855abfcc2f3b571
Benjamin Warner <benjaminwarner@u.boisestate.edu>
2018-09-07 17:26:33 -0600

    Add ability for the user to change the background color to cyan.

--- codepath-hello-world/app/src/main/java/com/example/benjamin/codepath_hello_world/MainActivity.java
+++ codepath-hello-world/app/src/main/java/com/example/benjamin/codepath_hello_world/MainActivity.java
@@ -2,8 +2,8 @@ package com.example.benjamin.codepath_hello_world;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,9 +16,16 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.changeTextColorButton).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.i("Ben", "Button click!");
                 TextView helloTextView = findViewById(R.id.helloTextView);
                 helloTextView.setTextColor(getResources().getColor(R.color.colorPrimaryDark));
+            }
+        });
+
+        findViewById(R.id.changeBackgroundColor).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                RelativeLayout rootView = findViewById(R.id.rootView);
+                rootView.setBackgroundColor(getResources().getColor(R.color.cyan));
             }
         });
     }
